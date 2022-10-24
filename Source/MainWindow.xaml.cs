1da2093baf4d8524ce9292b93afaf2e08b7cd567
t.novruzov <t.novruzov@STHQ0124-01>
2022-10-24 10:08:04 +0400

    Add project files.

---
+++ Source/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Controls.Ribbon;
+
+namespace Source;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
